e31a3b7ccd66408119f1c1241e4b9a40c5768d86
HanzM1 <hanzmedina13@gmail.com>
2024-01-31 13:59:41 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,4 +4,4 @@ public static Main {
 }
 }
 // 1st comment here placed here for further testing of jenkins, github/github desktop, vs studios
-// // Jenkins setup is next - more to follow after this
+// hard time with getting Jenkins setup 

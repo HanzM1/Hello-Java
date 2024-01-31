605701afd759cb2b270516d3edb73ab1d901f874
HanzM1 <hanzmedina13@gmail.com>
2024-01-31 13:41:51 -0500

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public static Main { 
+    public static void main(String[] args) {
+    System.out.println("Hello World in Jenkins");
+}
+}
+// 1st comment here placed here for further testing of jenkins, github/github desktop, vs studios
